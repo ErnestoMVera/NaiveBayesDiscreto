0a43f90a5d8ec8928cc7a7e38e32c01b409c0ab6
ErnestoVera <veraem.sw@gmail.com>
2021-11-02 18:47:13 -0700

    Implementado corrector laplaciano

--- bayes.m
+++ bayes.m
@@ -4,9 +4,10 @@ load('datos_wdbc.mat')
 %trainy = [1 2 1 2 1 2 1 2 2 1];
 %test = [1 2 1];
 %c = clasificadorNaiveBayes(test, train,trainy,2)
+corrector_laplaciano = 0.0001;
 inferidas = zeros(numel(testy),1);
 for i = 1:numel(testy)
-    inferidas(i) = clasificadorNaiveBayes(test(i,:), train, trainy, 2);
+    inferidas(i) = clasificadorNaiveBayes(test(i,:), train, trainy, 2, corrector_laplaciano);
 end
 fprintf("Porcentaje de reconocimiento 60%%-40%%: %.2f\n",sum(testy == inferidas)/numel(testy));
 [train,test,trainy,testy] = separar_datos(trn.xd,trn.y,[0.7 0.3],2);
@@ -16,7 +17,7 @@ fprintf("Porcentaje de reconocimiento 60%%-40%%: %.2f\n",sum(testy == inferidas)
 %c = clasificadorNaiveBayes(test, train,trainy,2)
 inferidas = zeros(numel(testy),1);
 for i = 1:numel(testy)
-    inferidas(i) = clasificadorNaiveBayes(test(i,:), train, trainy, 2);
+    inferidas(i) = clasificadorNaiveBayes(test(i,:), train, trainy, 2, corrector_laplaciano);
 end
 fprintf("Porcentaje de reconocimiento 70%%-30%%: %.2f\n",sum(testy == inferidas)/numel(testy));
 [train,test,trainy,testy] = separar_datos(trn.xd,trn.y,[0.8 0.2],2);
@@ -26,7 +27,7 @@ fprintf("Porcentaje de reconocimiento 70%%-30%%: %.2f\n",sum(testy == inferidas)
 %c = clasificadorNaiveBayes(test, train,trainy,2)
 inferidas = zeros(numel(testy),1);
 for i = 1:numel(testy)
-    inferidas(i) = clasificadorNaiveBayes(test(i,:), train, trainy, 2);
+    inferidas(i) = clasificadorNaiveBayes(test(i,:), train, trainy, 2, corrector_laplaciano);
 end
 fprintf("Porcentaje de reconocimiento 80%%-20%%: %.2f\n",sum(testy == inferidas)/numel(testy));
 [train,test,trainy,testy] = separar_datos(trn.xd,trn.y,[0.9 0.1],2);
@@ -36,6 +37,6 @@ fprintf("Porcentaje de reconocimiento 80%%-20%%: %.2f\n",sum(testy == inferidas)
 %c = clasificadorNaiveBayes(test, train,trainy,2)
 inferidas = zeros(numel(testy),1);
 for i = 1:numel(testy)
-    inferidas(i) = clasificadorNaiveBayes(test(i,:), train, trainy, 2);
+    inferidas(i) = clasificadorNaiveBayes(test(i,:), train, trainy, 2, corrector_laplaciano);
 end
 fprintf("Porcentaje de reconocimiento 90%%-10%%: %.2f\n",sum(testy == inferidas)/numel(testy));
--- clasificadorNaiveBayes.m
+++ clasificadorNaiveBayes.m
@@ -1,4 +1,4 @@
-function clase = clasificadorNaiveBayes(dato_test, datos_entrenamiento, clases, total_clases)
+function clase = clasificadorNaiveBayes(dato_test, datos_entrenamiento, clases, total_clases, corrector_laplaciano)
     pr_apriori = zeros(total_clases,1);
     for i = 1:total_clases
         pr_apriori(i) = numel(clases(clases == i))/numel(clases);
@@ -8,6 +8,7 @@ function clase = clasificadorNaiveBayes(dato_test, datos_entrenamiento, clases, 
     for i = 1:total_clases
         ci = datos_entrenamiento(clases == i,:);
         dens = sum((ci./dato_test) == 1)/(numel(clases(clases==i)));
+        dens(dens == 0) = corrector_laplaciano;
         pr_densidad(i) = prod(dens);
     end
     pr_c = pr_densidad.*pr_apriori;
