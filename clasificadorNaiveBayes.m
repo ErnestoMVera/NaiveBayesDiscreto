4a858ab6dbb28e319e2de401f9d3e8dbe5d2bb17
ErnestoVera <veraem.sw@gmail.com>
2021-11-02 18:31:05 -0700

    Clasificador naive bayes discreto completado

--- clasificadorNaiveBayes.m
+++ clasificadorNaiveBayes.m
@@ -1,1 +1,15 @@
-clase = 
+function clase = clasificadorNaiveBayes(dato_test, datos_entrenamiento, clases, total_clases)
+    pr_apriori = zeros(total_clases,1);
+    for i = 1:total_clases
+        pr_apriori(i) = numel(clases(clases == i))/numel(clases);
+    end
+    % Densidad para el datoTest dado.
+    pr_densidad = zeros(total_clases,1);
+    for i = 1:total_clases
+        ci = datos_entrenamiento(clases == i,:);
+        dens = sum((ci./dato_test) == 1)/(numel(clases(clases==i)));
+        pr_densidad(i) = prod(dens);
+    end
+    pr_c = pr_densidad.*pr_apriori
+    clase = find(pr_c == max(pr_c));
+end
